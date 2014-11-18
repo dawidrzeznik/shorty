bcc0f0c30011b8e1af36c84fed9fe98761355639
dawidziu <dawid.rzeznik@gmail.com>
2014-11-19 00:34:38 +0100

    shorten urls

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorty::Application.config.secret_token = 'ba80734d3b04e868421edc53826564b1be66fc85cb86dfb47f8719fc382fb749b65714dd1c8fc275a0b465baf6d885f30f72e4f525b8d20b7aced343c156e551'
